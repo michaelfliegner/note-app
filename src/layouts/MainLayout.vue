07181bf03db184fad94b81e77e72ef8d3f195400
Michael Fliegner <michael.fliegner@hamburg.de>
2022-04-26 17:06:19 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Note Taking
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
